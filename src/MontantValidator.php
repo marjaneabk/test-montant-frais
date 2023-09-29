c8d5c34e0540ceb51ad3acd727145140e7b6fc70
marjaneAbk <marjane.aberkan@lmdsio.fr>
2023-09-29 15:03:54 +0200

    Initial commit

---
+++ src/MontantValidator.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace maber\TestMontantFrais;
+
+class MontantValidator{
+    public static function validerMontant($montant){
+        return is_numeric($montant) && $montant>= 0.01 && $montant <= 200;
+    }
+}
